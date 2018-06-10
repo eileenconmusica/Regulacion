83edf86d5d71ad60d38ad3b58f8be6db5162d14d
eileenconmusica <irenegordillo@gmail.com>
2018-06-10 11:30:03 +0200

    Segunda modificación

--- BaseScript.m
+++ BaseScript.m
@@ -3,11 +3,11 @@
 % Componentes del grupo
 % Alarcón Sánchez, Francisco
 % Gordillo Bosque, Irene
-% Pérez __, Alejandro
+% Pérez Cobaleda, Alejandro
 % 
 %
 %
-%% Generación de parámetros y obtención de la función de trasferencia
+%% 1-2 Generación de parámetros y obtención de la función de trasferencia
 clear;
 clc;
 [Datos, G]=GeneraParametros([2 5 3 5 1 6 8 0]);
@@ -20,12 +20,22 @@ r=Datos(5);
 % que su masa es:
 Mc=2*J/r^2;
 
-%% Función de transferencia discretizada
+%% 3 Función de transferencia discretizada
 Gd=c2d(G,0.1);
 % (((( Habría que hacer dibujillos y eso)))
-%% Space State
+%% 4 Space State 
 [num, den]=tfdata(G, 'v');
-[A B C D]=tf2ss(num, den);
-%sys=[A B C D];
+[A,B,C,D]=tf2ss(num, den);
+sysC=ss(A,B,C,D);
 % La salida es la posición x
+% DSS T=0.1
+[d1num, d1den]=tfdata(Gd, 'v');
+[Gd1,Hd1,Cd1,Dd1]=tf2ss(d1num, d1den);
+sysD1=ss(Gd1,Hd1,Cd1,Dd1);
+% DSS T=1
+Gd2=c2d(G,1);
+[d2num, d2den]=tfdata(Gd2, 'v');
+[G,H,C,D]=tf2ss(d2num, d2den);
+sysD2=ss(A,B,C,D);
+
 
